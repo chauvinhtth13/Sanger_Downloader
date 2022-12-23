6910ca6fa771b4ee30af48c80fe132deb9e392dc
VincentZhu <40750050+chauvinhtth13@users.noreply.github.com>
2022-12-23 16:30:03 +0700

    second update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Sanger_Downloader
-using for download data on Monocle server
+# Sanger Downloader: using for download data on Sanger database
+
+## Build Requirements
+The following packages are needed to run the software
++ python >= 3.7.0
++ codecs
++ BeautifulSoup
++ selenium
++ webdriver_manager
+## Install
+Clone repo and install requirements.txt in a Python>=3.7.0 environment
+```commandline
+git clone https://github.com/chauvinhtth13/Sanger_Downloader.git
+cd yolov5
+pip install -r requirements.txt
+```
+## Usage
+```commandline
+usage: python Sanger_Downloader.py [-i file_html]
+
+Input/Output:
+    -i file_html, --input_file file_html : input file html have link download file
+```
+
--- Sanger_Downloader.py
+++ Sanger_Downloader.py
@@ -1,104 +1,13 @@
 import argparse
-import requests
 import codecs
 from bs4 import BeautifulSoup
-import os
-import re
-import sys
-from sys import platform
-from urllib import request
-import zipfile
 from selenium import webdriver
-
-ZIP_UNIX_SYSTEM = 3
-
-
-def extract_all_with_permission(zf, target_dir):
-  for info in zf.infolist():
-    extracted_path = zf.extract(info, target_dir)
-
-    if info.create_system == ZIP_UNIX_SYSTEM:
-      unix_attributes = info.external_attr >> 16
-      if unix_attributes:
-        os.chmod(extracted_path, unix_attributes)
-
-
-def extract_version_registry(output):
-    try:
-        google_version = ''
-        for letter in output[output.rindex('DisplayVersion    REG_SZ') + 24:]:
-            if letter != '\n':
-                google_version += letter
-            else:
-                break
-        return(google_version.strip())
-    except TypeError:
-        return
-
-
-def extract_version_folder():
-    for i in range(2):
-        path = 'C:\\Program Files' + \
-            (' (x86)' if i else '') + '\\Google\\Chrome\\Application'
-        if os.path.isdir(path):
-            paths = [f.path for f in os.scandir(path) if f.is_dir()]
-            for path in paths:
-                filename = os.path.basename(path)
-                pattern = '\d+\.\d+\.\d+\.\d+'
-                match = re.search(pattern, filename)
-                if match and match.group():
-                    # Found a Chrome version.
-                    return match.group(0)
-    return None
-
-
-def get_chrome_version():
-    version = None
-    install_path = None
-    download_path = None
-    link_download = "https://chromedriver.storage.googleapis.com/"
-
-    try:
-        if platform == "linux" or platform == "linux2":
-            # linux
-            install_path = "/usr/bin/google-chrome"
-            download_path = "/chromedriver_linux64.zip"
-        elif platform == "darwin":
-            # OS X
-            install_path = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-            download_path = "/chromedriver_mac64.zip"
-        elif platform == "win32" or platform == "win64":
-            # Windows...
-            try:
-                # Try registry key.
-                stream = os.popen(
-                    'reg query "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome"')
-                output = stream.read()
-                version = extract_version_registry(output)
-            except Exception:
-                version = extract_version_folder()
-            download_path = "/chromedriver_win64.zip"
-    except Exception as ex:
-        print(ex)
-
-    version = os.popen(f"{install_path} --version").read().strip(
-        'Google Chrome ').strip() if install_path else version
-
-    split_version = version.split(".")
-
-    final_version = ".".join(split_version[0:len(split_version)-1])
-    version_download = requests.get(
-        "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_" + final_version)
-
-    request.urlretrieve(link_download+version_download.text +
-                        download_path, "chromedriver.zip")
-    with zipfile.ZipFile("chromedriver.zip", 'r') as zip_ref:
-        extract_all_with_permission(zip_ref, ".")
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Automatic download data on Sanger database by Google Chrome. Made by Vinh Chau ("
-                                                 "chauvinhtth13@gmail.com)")
+    parser = argparse.ArgumentParser(description="Automatic download data on Sanger database by Google Chrome. "
+                                                 "Made by Vinh Chau (chauvinhtth13@gmail.com)")
     parser.add_argument("-i", "--input_html", type=str, default="",
                         help="Input file HTML")
     args = parser.parse_args()
@@ -107,15 +16,7 @@ if __name__ == "__main__":
     HTMLFile = open(html_file, "r")
     index = HTMLFile.read()
     Soup_code = BeautifulSoup(index, 'lxml')
-    get_chrome_version()
-    try:
-        if platform == "linux" or platform == "linux2" or platform == "darwin":
-            browser = webdriver.Chrome('./chromedriver')
-        elif platform == "win32":
-            browser = webdriver.Chrome('./chromedriver.exe')
-        for i in Soup_code.findAll("a", attrs={"class": "download-link"}):
-            print(i["href"])
-            browser.get(i["href"])
-    except Exception:
-        print("Can not detect platform OS. (script support Mac,Linux,Window")
-        sys.exit(0)
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager(path=r".").install()))
+    for i in Soup_code.findAll("a", attrs={"class": "download-link"}):
+        print(i["href"])
+        browser.get(i["href"])
