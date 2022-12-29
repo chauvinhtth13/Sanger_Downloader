a93d148ecce06e0a2c0cd19c28e3bfba367afe9e
VincentZhu <40750050+chauvinhtth13@users.noreply.github.com>
2022-12-29 11:28:57 +0700

    Update Sanger_Downloader.py

--- Sanger_Downloader.py
+++ Sanger_Downloader.py
@@ -3,6 +3,7 @@ import codecs
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 if __name__ == "__main__":
@@ -10,13 +11,26 @@ if __name__ == "__main__":
                                                  "Made by Vinh Chau (chauvinhtth13@gmail.com)")
     parser.add_argument("-i", "--input_html", type=str, default="",
                         help="Input file HTML")
+    parser.add_argument("-p", "--download_directory", type=str, default="",
+                        help="Input file HTML")
     args = parser.parse_args()
     html_file = args.input_html
+
     f = codecs.open(html_file, 'r', 'utf-8')
     HTMLFile = open(html_file, "r")
     index = HTMLFile.read()
+
     Soup_code = BeautifulSoup(index, 'lxml')
+    chrome_options = Options()
+    if args.download_directory != "":
+        chrome_options.add_experimental_option("prefs", {
+            "download.default_directory": args.download_directory,
+        })
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+
     browser = webdriver.Chrome(service=Service(ChromeDriverManager(path=r".").install()))
+
     for i in Soup_code.findAll("a", attrs={"class": "download-link"}):
         print(i["href"])
         browser.get(i["href"])
